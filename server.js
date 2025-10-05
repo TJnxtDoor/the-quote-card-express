c1654157bdf95fa66dd6e9ddd9f5c8cc84d2f2c8
TJ <afzaltj81@gmail.com>
2025-10-05 13:15:03 -0400

    r

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 
+  const port = 3000;
 app.use(express.static("./public"));
 app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
