3dd8597bed23eb5b6f4db8f2fe6ab9bebfd27ed7
TJ <afzaltj81@gmail.com>
2025-10-05 12:27:53 -0400

    test

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 
 const express = require("express");
 const app = express();
-const port = 8080;
+const path = require("path");
+const port = process.env.PORT || 
 app.use(express.static("./public"));
 app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}));
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
